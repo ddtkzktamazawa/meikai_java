764c044192caa0bd59cadec2efb2105761da9df4
ddtkzktamazawa <ddt.kzk.tamazawa@gmail.com>
2026-04-07 15:31:24 +0900

    演習4-4終わりました

---
+++ Question4_4/src/Question4_4/Question4_4.java
@@ -0,0 +1,44 @@
+package Question4_4;
+import java.util.Scanner;
+/*
+ * クラス名:Question4_4
+ * 概要    :正の整数値を-1までカウントダウン
+ * 作成者  :玉澤一輝
+ * 作成日  :2026.4.7
+ */
+public class Question4_4 {
+	 /**
+     * 関数名           :main
+     * メソッドの説明   :正の整数値を-1までカウントダウン
+     * パラメータの説明 :なし
+     * 返り値           :なし
+     * 作成者           :玉澤一輝
+     * 作成日           :2026.4.7
+     */
+	public static void main(String[] args) {
+		//キーボードから読み込みを行う
+		Scanner standardInput = new Scanner(System.in);
+		//"カウントダウンします。と表示して改行する"
+		System.out.println("カウントダウンします。");
+		//入力した数
+		int timeleft;
+		//実行する
+		do {
+			//"正の整数値"と表示する
+			System.out.print("正の整数値：");
+			//rimeleftに数w読み込む
+			timeleft = standardInput.nextInt();
+			//timeleftが0以下ならばdo文を再実行する
+		}while(timeleft <= 0);
+		//timeleftが0以上であれば
+		while(timeleft >= -1) {
+		//"timeleft"を表示して改行する
+		System.out.println(timeleft);
+		//timeleftの値をデクリメント
+		timeleft--;
+		
+		}
+
+	}
+
+}
