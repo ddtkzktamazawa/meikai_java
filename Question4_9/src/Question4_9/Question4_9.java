c24c9ade28c6ceede2002d4ff6b5c099ad0a0ff8
ddtkzktamazawa <ddt.kzk.tamazawa@gmail.com>
2026-04-09 11:54:08 +0900

    演習4-9終わりました

---
+++ Question4_9/src/Question4_9/Question4_9.java
@@ -0,0 +1,46 @@
+package Question4_9;
+import java.util.Scanner;
+/*
+ * クラス名:Question4_8
+ * 概要    :正の整数値を読み込んで1からその値までの積を求める
+ * 作成者  :玉澤一輝
+ * 作成日  :2026.4.9
+ */
+public class Question4_9 {
+	/**
+	 * 関数名           :main
+	 * メソッドの説明   :正の整数値を読み込んで1からその値までの積を求める
+	 * パラメータの説明 :なし
+	 * 返り値           :なし
+	 * 作成者           :玉澤一輝
+	 * 作成日           :2026.4.9
+	 */
+	public static void main(String[] args) {
+		//キーボードから読み込みを行う
+		Scanner standardInput = new Scanner(System.in);
+		//変数choiceNumber
+		int choiceNumber;
+		//実行する
+		do {
+			//"nの値："と表示する
+			System.out.print("nの値：");
+			//choiceNumberに数を読み込む
+			choiceNumber = standardInput.nextInt();
+			//choiceNumberが0以下ならばdo文を再実行する
+		}while(choiceNumber <= 0);
+		//productTotalに1を代入する
+		int productTotal = 1;
+		//incrementValueに1を代入する
+		int incrementValue =1;
+		//incrementValueがchoiceNumber以下であれば
+		while(incrementValue <= choiceNumber) {
+			//productTotalにincrementValueをかける
+			productTotal *= incrementValue;
+			//incrementValueの値をインクリメントする
+			incrementValue++; 
+		}
+		//"1からchoiceNumberまでの積はproductTotalです。"と表示する
+		System.out.print("1から"+choiceNumber+"までの積は"+productTotal+"です。");
+	}
+
+}
