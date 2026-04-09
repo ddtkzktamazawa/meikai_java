3477c27d1a6b378429a9aaad531a9d7843753b60
ddtkzktamazawa <ddt.kzk.tamazawa@gmail.com>
2026-04-09 11:37:56 +0900

    演習4-7の指摘部分、修正しました

--- Question4_7/src/Question4_7/Question4_7.java
+++ Question4_7/src/Question4_7/Question4_7.java
@@ -8,48 +8,45 @@ import java.util.Scanner;
  */
 public class Question4_7 {
 	/**
-     * 関数名           :main
-     * メソッドの説明   :読み込んだ個数だけ記号文字を表示（*と+を交互に表示）
-     * パラメータの説明 :なし
-     * 返り値           :なし
-     * 作成者           :玉澤一輝
-     * 作成日           :2026.4.8
-     */
+	 * 関数名           :main
+	 * メソッドの説明   :読み込んだ個数だけ記号文字を表示（*と+を交互に表示）
+	 * パラメータの説明 :なし
+	 * 返り値           :なし
+	 * 作成者           :玉澤一輝
+	 * 作成日           :2026.4.8
+	 */
 	public static void main(String[] args) {
 		//キーボードから読み込みを行う
 		Scanner standardInput = new Scanner(System.in);
 		//"何個表示しますか"と表示する
 		System.out.print("何個表示しますか：");
-		//numberAに入力した数を読み込み
-		int numberA = standardInput.nextInt();
+		//numberFirstに入力した数を読み込み
+		int numberFirst = standardInput.nextInt();
 		//incrementValueに0を代入する
 		int incrementValue = 0;
-		//numberBにnumberA/2を代入する
-		int numberB = numberA / 2;
-		//もしnumberAを2で割った剰余が0の場合
-		if(numberA % 2 == 0) {
-			//""を表示する
-			System.out.print("");
-		//その他の場合
-		}else {
+		//numberSecondにnumberFirst/2を代入する(*と+を1つのまとまりとして見る)
+		int numberSecond = numberFirst / 2;
+		//もしnumberFirstを2で割った剰余が1の場合
+		if(numberFirst % 2 == 1) {
 			//"*"を表示する
 			System.out.print('*');
 		}
-		//incrementValueがnumberB未満であれば
-		while(incrementValue < numberB) {
-			//もしmunberAを2で割った剰余が0の場合
-			if(numberA % 2 == 0)
+		//incrementValueがnumberSecond未満であれば
+		while(incrementValue < numberSecond) {
+			//もしmunberFirstを2で割った剰余が0の場合
+			if(numberFirst % 2 == 0) {
 				//"*+"を表示する
 				System.out.print("*+");
-			//その他の場合
-			else
+				//その他の場合
+			}else {
 				//"+*"を表示する
 				System.out.print("+*");
-			//incrementValueをインクリメントする
-		incrementValue++;
+				//incrementValueをインクリメントする
+			}
+			incrementValue++;
 		}
-		//""を表示して改行する
-		System.out.println();
+		//""を表示する
+		System.out.print("");
 
 	}
 
