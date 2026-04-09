6afe1b99abade3575d9bf7c8f7fd13409694726c
ddtkzktamazawa <ddt.kzk.tamazawa@gmail.com>
2026-04-09 16:15:17 +0900

    演習4-11終わりました

---
+++ Question4_11/src/Question4_11/Question4_11.java
@@ -0,0 +1,40 @@
+package Question4_11;
+import java.util.Scanner;
+/*
+ * クラス名:Question4_11
+ * 概要    :正の整数値を0までカウントダウン
+ * 作成者  :玉澤一輝
+ * 作成日  :2026.4.9
+ */
+public class Question4_11 {
+	 /**
+     * 関数名           :main
+     * メソッドの説明   :正の整数値を0までカウントダウン
+     * パラメータの説明 :なし
+     * 返り値           :なし
+     * 作成者           :玉澤一輝
+     * 作成日           :2026.4.9
+     */
+	public static void main(String[] args) {
+		//キーボードから読み込みを行う
+		Scanner standardInput = new Scanner(System.in);
+		//"カウントダウンします。と表示して改行する
+		System.out.println("カウントダウンします。");
+		//変数choiceNumber
+		int countDown;
+		//実行する
+		do {
+			//"正の整数値"と表示する
+			System.out.print("正の整数値：");
+			//countDownに数を読み込む
+			countDown = standardInput.nextInt();
+			//countDownが0以下ならばdo文を再実行する
+		}while(countDown <= 0);
+		//変数incrementValueに0を代入してcountDownを1つずつ減らしながらcountDown回繰り返す
+		for(int incrementValue = 0; incrementValue <= countDown; countDown--)
+			//"countDown"を表示して改行する
+			System.out.println(countDown);
+		
+	}
+
+}
