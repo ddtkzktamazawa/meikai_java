1601b62492926c0abe8a65fae9b506a468d58b42
ddtkzktamazawa <ddt.kzk.tamazawa@gmail.com>
2026-04-13 13:09:52 +0900

    演習4-16終わりました

--- Question4_16/src/Question4_16/Question4_16.java
+++ Question4_16/src/Question4_16/Question4_16.java
@@ -28,17 +28,17 @@ public class Question4_16 {
 			inputNumber = standardInput.nextInt();
 			//入力した数が0以下ならばdo文を再実行する
 		}while(inputNumber < 0);
-		////変数iを1から始めて1つずつ増やしながら入力した数の回数ループ本体を繰り返す
+		//変数iを1から始めて1つずつ増やしながら入力した数の回数ループ本体を繰り返す
 		for(int i = 1; i <= inputNumber; i++) {
 			//'*'を表示する
-			System.out.print("*");
+			System.out.print('*');
 			//もしiを5で割った剰余が0の場合
-		if(i % 5 == 0) {
-			//改行する
-			System.out.println();
+			if(i % 5 == 0) {
+				//改行する
+				System.out.println();
+			}
 		}
 	}
-	}
-	
+
 
 }
