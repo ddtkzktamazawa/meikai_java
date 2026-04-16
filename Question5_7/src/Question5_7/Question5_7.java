d1753ca0672deaabb9881db4f7a6caf37543146b
ddtkzktamazawa <ddt.kzk.tamazawa@gmail.com>
2026-04-16 14:53:42 +0900

    演習5-4,5-5修正しました。

--- Question5_7/src/Question5_7/Question5_7.java
+++ Question5_7/src/Question5_7/Question5_7.java
@@ -22,7 +22,7 @@ public class Question5_7 {
 		//変数iを0.0から1.0まで0.001ずつ増やす
 		for(float i = 0.0F; i <= 1.0F; i += 0.001F) {
 			//小数点以下7桁の変数iと変数iの二乗の値を表示する
-			System.out.printf("%8.7f  %8.7f\n",i, i * i);
+			System.out.printf("%8.7f  %8.7f\n", i, i * i);
 		}
 	}
 }
