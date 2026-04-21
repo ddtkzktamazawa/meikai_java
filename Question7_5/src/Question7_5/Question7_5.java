6b3db112582fc3ce64acc00c439b9df0384793a5
ddtkzktamazawa <ddt.kzk.tamazawa@gmail.com>
2026-04-21 17:14:57 +0900

    演習7-5終わりました

---
+++ Question7_5/src/Question7_5/Question7_5.java
@@ -0,0 +1,25 @@
+package Question7_5;
+/*
+ * クラス名:Question7_5
+ * 概要    :「こんにちは。」と表示する
+ * 作成者  :玉澤一輝
+ * 作成日  :2026.4.21
+ */
+public class Question7_5 {
+	/**
+	 * 関数名           :main
+	 * メソッドの説明   :「こんにちは。」と表示する
+	 * パラメータの説明 :なし
+	 * 返り値           :なし
+	 * 作成者           :玉澤一輝
+	 * 作成日           :2026.4.21
+	 */
+	static  void hello() {
+		//"こんにちは。"を表示して改行する
+		System.out.println("こんにちは。");
+	}
+	public static void main(String[] args) {
+		//helloメソッドの呼び出し
+		hello();
+	}
+}
