b3157e23b7167eb99ae7b2651f65b457ca8fc3ab
ddtkzktamazawa <ddt.kzk.tamazawa@gmail.com>
2026-04-22 15:01:52 +0900

    演習7-10終わりました

--- Question7_10/src/Question7_10/Question7_10.java
+++ Question7_10/src/Question7_10/Question7_10.java
@@ -1,59 +1,114 @@
 package Question7_10;
 import java.util.Random;
 import java.util.Scanner;
-
+/*
+ * クラス名            :Question7_10
+ * 概要                :四つの問題からランダムに出題する暗算ゲームの作成
+ * 作成者              :玉澤一輝
+ * 作成日              :2026.4.22
+ */
 public class Question7_10 {
+	
 	static Scanner standardInput = new Scanner(System.in);
-
+	/*
+	 * 関数名           :confirmRetry
+	 * メソッドの説明   :もう一度行うか0,1を入力しtrue,falseを返却する
+	 * パラメータの説明 :なし
+	 * 返り値           :true,false
+	 * 作成者           :玉澤一輝
+	 * 作成日           :2026.4.22
+	 */
 	static boolean confirmRetry() {
-		int continueNumber;
+		//変数continueNumberの初期化
+		int continueNumber = 0;
+		//実行する
 		do {
+			//"もう一度？＜Yes…1/No…0＞："と表示する
 			System.out.print("もう一度？＜Yes…1/No…0＞：");
+			//continueNumberに入力した値を代入する
 			continueNumber = standardInput.nextInt();
+			//0と1以外の場合繰り返す
 		}while(continueNumber != 0 && continueNumber != 1);
+		//1であればtrue、0であればfalseを返却する
 		return continueNumber == 1;
 	}
+	/*
+	 * 関数名           :main
+	 * メソッドの説明   :三桁のランダムな値を出力し四つの問題からランダムに選ばれ計算結果を入力し表示する
+	 * パラメータの説明 :なし
+	 * 返り値           :なし
+	 * 作成者           :玉澤一輝
+	 * 作成日           :2026.4.22
+	 */
 	public static void main(String[] args) {
+		//ランダムな数を入れる変数を作成
 		Random randomNumber = new Random();
-
+		//"暗算力トレーニング!!"を表示する
 		System.out.print("暗算力トレーニング!!");
+		//実行する
 		do {
+			//firstNumberに三桁の値を代入
 			int firstNumber = randomNumber.nextInt(900) + 100;
+			//secondNumberに三桁の値を代入
 			int secondNumber = randomNumber.nextInt(900) + 100;
+			//thirdNumberに三桁の値を代入
 			int thirdNumber = randomNumber.nextInt(900) + 100;
+			//0-3のランダムな値を代入
 			int questionStatement = randomNumber.nextInt(4);
-			
+			//true以外の場合繰り返す
 			while(true) {
+				//もしquestionStatementが0の場合
 				if(questionStatement == 0) {
+					//"firstNumber + secondNumber + thirdNumber ="と表示する
 					System.out.print(firstNumber + "+" + secondNumber + "+" + thirdNumber + "=");
+					//sumNumberに入力した値を代入する
 					int sumNumber = standardInput.nextInt();
+					//もしsumNumberとfirstNumber + secondNumber + thirdNumberが一致した場合
 					if(sumNumber == firstNumber + secondNumber + thirdNumber) {
+						//飛び出す
 						break;
 					}
 				}
+				//もしquestionStatementが1の場合
 				else if(questionStatement == 1) {
+					//"firstNumber + secondNumber - thirdNumber ="と表示する
 					System.out.print(firstNumber + "+" + secondNumber + "-" + thirdNumber + "=");
+					//sumNumberに入力した値を代入する
 					int sumNumber = standardInput.nextInt();
+					//もしsumNumberとfirstNumber + secondNumber - thirdNumberが一致した場合
 					if(sumNumber == firstNumber + secondNumber - thirdNumber) {
+						//飛び出す
 						break;
 					}
 				}
+				//もしquestionStatementが2の場合
 				else if(questionStatement == 2) {
+					//"firstNumber - secondNumber + thirdNumber ="と表示する
 					System.out.print(firstNumber + "-" + secondNumber + "+" + thirdNumber + "=");
+					//sumNumberに入力した値を代入する
 					int sumNumber = standardInput.nextInt();
+					//もしsumNumberとfirstNumber - secondNumber + thirdNumberが一致した場合
 					if(sumNumber == firstNumber - secondNumber + thirdNumber) {
+						//飛び出す
 						break;
 					}
 				}
+				//それ以外の場合
 				else{
+					//"firstNumber - secondNumber - thirdNumber ="と表示する
 					System.out.print(firstNumber + "-" + secondNumber + "-" + thirdNumber + "=");
+					//sumNumberに入力した値を代入する
 					int sumNumber = standardInput.nextInt();
+//もしsumNumberとfirstNumber - secondNumber - thirdNumberが一致した場合
 					if(sumNumber == firstNumber - secondNumber - thirdNumber) {
+						//飛び出す
 						break;
 					}
 				}
+				//"違いますよ!!"と表示して改行する
 				System.out.println("違いますよ!!");
 			}
+			//trueの場合繰り返す
 		}while(confirmRetry());
 	}
 }
