945c6fbd6c8965033a62d686df0d2224a4b0f1a7
ddtkzktamazawa <ddt.kzk.tamazawa@gmail.com>
2026-04-22 11:35:57 +0900

    演習7-8終わりました

--- Question7_8/src/Question7_8/Question7_8.java
+++ Question7_8/src/Question7_8/Question7_8.java
@@ -16,27 +16,37 @@ public class Question7_8 {
 	 * 作成日           :2026.4.22
 	 */
 	static int random(int firstRamdom, int secondRamdom) {
+		//ランダムな数を入れる変数を作成
 		Random randomNumber = new Random();
+		//変数thirdRamdomの初期化
 		int thirdRamdom = 0;
+		//もしランダム数1よりランダム数2の値が大きい場合
 		if(firstRamdom < secondRamdom) {	
+			//ランダム数1以上ランダム数2未満の乱数をthirdRamdomに代入して返却
 			return thirdRamdom = randomNumber.nextInt(secondRamdom - firstRamdom) + firstRamdom;		
 		}
+		//その他の場合
 		else {
+			//firstRamdomの値をthirdRamdomに代入して返却
 			return thirdRamdom = firstRamdom;
 		}
 	}
 	/*
 	 * 関数名           :main
-	 * メソッドの説明   :0-99のランダムな数を出力しrandomのメソッドの結果を表示する
+	 * メソッドの説明   :readPlusIntメソッドの返却値を受け取る
 	 * パラメータの説明 :なし
 	 * 返り値           :なし
 	 * 作成者           :玉澤一輝
 	 * 作成日           :2026.4.22
 	 */
 	public static void main(String[] args) {
+		//ランダムな数を入れる変数を作る
 		Random randomNumber = new Random();
+		//firstRamdomに0-99のランダムな値を代入する
 		int firstRamdom = randomNumber.nextInt(100);
+		//secondRamdomに0-99のランダムな値を代入する
 		int secondRamdom = randomNumber.nextInt(100);
+		//"ランダムな数：random(firstRamdom, secondRamdom)"表示して改行する
 		System.out.println("ランダムな数：" + random(firstRamdom, secondRamdom));
 	}
 }
