507fe1bc53ab16b3080698d2e2f78891bcae5702
ddtkzktamazawa <ddt.kzk.tamazawa@gmail.com>
2026-04-23 09:46:02 +0900

    演習7-8修正しました

--- Question7_8/src/Question7_8/Question7_8.java
+++ Question7_8/src/Question7_8/Question7_8.java
@@ -9,27 +9,24 @@ import java.util.Random;
 public class Question7_8 {
 	/*
 	 * 関数名           :random
-	 * メソッドの説明   :ランダム数1以上ランダム数2未満の乱数を生成して返却
+	 * メソッドの説明   :第一引数が第二引数より小さければ第一引数以上第二引数未満の乱数を生成して返却し、
+	 *                   第一引数が第二引数以上であれば第一引数を返却する
 	 * パラメータの説明 :ランダム数1とランダム数2
-	 * 返り値           :ランダム数1かランダム数1以上ランダム数2の値
+	 * 返り値           :第一引数が第二引数より小さければ第一引数以上第二引数未満の乱数、
+	 *                   第一引数が第二引数以上であれば第一引数
 	 * 作成者           :玉澤一輝
 	 * 作成日           :2026.4.22
 	 */
-	static int random(int firstRamdom, int secondRamdom) {
+	static int random(int firstRandom, int secondRandom) {
 		//ランダムな数を入れる変数を作成
 		Random randomNumber = new Random();
-		//変数thirdRamdomの初期化
-		int thirdRamdom = 0;
-		//もしランダム数1よりランダム数2の値が大きい場合
-		if(firstRamdom < secondRamdom) {	
-			//ランダム数1以上ランダム数2未満の乱数をthirdRamdomに代入して返却
-			return thirdRamdom = randomNumber.nextInt(secondRamdom - firstRamdom) + firstRamdom;		
+		//もしランダム数2よりランダム数1の値が大きい場合
+		if(firstRandom >= secondRandom) {	
+			//ランダム数1を返却する
+			return firstRandom;
 		}
-		//その他の場合
-		else {
-			//firstRamdomの値をthirdRamdomに代入して返却
-			return thirdRamdom = firstRamdom;
-		}
+		//(secondRandom - firstRandom) + firstRandom)内の乱数を計算して返却する
+		return randomNumber.nextInt(secondRandom - firstRandom) + firstRandom;
 	}
 	/*
 	 * 関数名           :main
@@ -42,11 +39,11 @@ public class Question7_8 {
 	public static void main(String[] args) {
 		//ランダムな数を入れる変数を作る
 		Random randomNumber = new Random();
-		//firstRamdomに0-99のランダムな値を代入する
-		int firstRamdom = randomNumber.nextInt(100);
-		//secondRamdomに0-99のランダムな値を代入する
-		int secondRamdom = randomNumber.nextInt(100);
-		//"ランダムな数：random(firstRamdom, secondRamdom)"表示して改行する
-		System.out.println("ランダムな数：" + random(firstRamdom, secondRamdom));
+		//firstRandomに0-99のランダムな値を代入する
+		int firstRandom = randomNumber.nextInt(100);
+		//secondRandomに0-99のランダムな値を代入する
+		int secondRandom = randomNumber.nextInt(100);
+		//"ランダムな数：random(firstRandom, secondRandom)"表示して改行する
+		System.out.println("ランダムな数：" + random(firstRandom, secondRandom));
 	}
 }
